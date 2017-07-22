31e63ac6ad06ee82d51813c868a902928680da69
Tennant Shaw <tennantshaw@Macs-MacBook-Pro.local>
2017-07-21 22:46:31 -0700

    added player node to scene.

--- The Penguins Escape/GameScene.swift
+++ The Penguins Escape/GameScene.swift
@@ -13,17 +13,15 @@ class GameScene: SKScene {
     // Create a constant cam as SKCameraNode:
     let cam = SKCameraNode()
     let ground = Ground()
-    // Create our bee node as a property of GameScene so we can access it throughout the class
-    let bee = SKSpriteNode()
-
+    let player = Player()
+    
+    
     // MARK: - View Life Cycle
     override func didMove(to view: SKView) {
         self.anchorPoint = .zero
         self.backgroundColor = UIColor(red: 0.4, green: 0.6, blue: 0.95, alpha: 1.0)
         // Assign the camera to the scene
         self.camera = cam
-        // Call the new bee function
-        self.addTheFlyingBee()
         
         // Add a second Bee to the scene:
         let bee2 = Bee()
@@ -44,45 +42,16 @@ class GameScene: SKScene {
         ground.createChildren()
         // Add the ground node to the scene:
         self.addChild(ground)
+        
+        // Position the player:
+        player.position = CGPoint(x: 150, y: 250)
+        // Add the player node to the scene:
+        self.addChild(player)
     }
     
     override func didSimulatePhysics() {
         // Keep the camera centered on the bee
-        self.camera!.position = bee.position
-    }
-    func addTheFlyingBee() {
-        // Position our bee
-        bee.position = CGPoint(x: 250, y: 250)
-        bee.size = CGSize(width: 28, height: 24)
-        // Add the bee to the scene
-        self.addChild(bee)
-        
-        // Find the bee textures from the texture atlas
-        let beeAtlas = SKTextureAtlas(named: "Enemies")
-        let beeFrames: [SKTexture] = [
-            beeAtlas.textureNamed("bee"),
-            beeAtlas.textureNamed("bee-fly")
-        ]
-        // Create a new SKAction to animate between the frames
-        let flyAction = SKAction.animate(with: beeFrames, timePerFrame: 0.14)
-        // Create an SKAction to run the flyAction repeatedly
-        let beeAction = SKAction.repeatForever(flyAction)
-        // Instruct our bee to run the final repeat action:
-        bee.run(beeAction)
-        
-        // Set up new actions to move our bee back and forth:
-        let pathLeft = SKAction.moveBy(x: -200, y: -10, duration: 2)
-        let pathRight = SKAction.moveBy(x: 200, y: 10, duration: 2)
-        let flipTextureNegative = SKAction.scaleX(to: -1, duration: 0)
-        let flipTexturePositive = SKAction.scaleX(to: 1, duration: 0)
-        // Combine actions into a cohesive flight sequence
-        let flightOfTheBee = SKAction.sequence([pathLeft, flipTextureNegative,
-                                               pathRight, flipTexturePositive])
-        // Create a looping action that will repeat forever:
-        let neverEndingFlight = SKAction.repeatForever(flightOfTheBee)
-        // Tell the bee to run the flight path
-        bee.run(neverEndingFlight)
-
+        self.camera!.position = player.position
     }
     
 }
