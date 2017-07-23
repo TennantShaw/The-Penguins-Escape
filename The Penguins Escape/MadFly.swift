d7a71eb36080b3dd2c16bf1a589f3addac0009e8
Tennant Shaw <tennantshaw@Macs-MacBook-Pro.local>
2017-07-23 13:32:53 -0700

    added madfly class

---
+++ The Penguins Escape/MadFly.swift
@@ -0,0 +1,43 @@
+//
+//  MadFly.swift
+//  The Penguins Escape
+//
+//  Created by Tennant Shaw on 7/23/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+import SpriteKit
+
+class MadFly: SKSpriteNode, GameSprite {
+    // MARK: - Properties
+    var initialSize = CGSize(width: 61, height: 29)
+    var textureAtlas: SKTextureAtlas = SKTextureAtlas(named: "Enemies")
+    var flyAnimation = SKAction()
+    
+    
+    // MARK: - Initializers
+    init() {
+        super.init(texture: nil, color: .clear, size: initialSize)
+        self.physicsBody = SKPhysicsBody(circleOfRadius: size.width / 2)
+        self.physicsBody?.affectedByGravity = false
+        createAnimations()
+        self.run(flyAnimation)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    // MARK: - Methods
+    func createAnimations() {
+        let flyFrames: [SKTexture] = [
+        textureAtlas.textureNamed("madfly"),
+        textureAtlas.textureNamed("madfly-fly")
+        ]
+        let flyAction = SKAction.animate(with: flyFrames, timePerFrame: 0.14)
+        flyAnimation = SKAction.repeatForever(flyAction)
+    }
+    
+    func onTap() { }
+}
